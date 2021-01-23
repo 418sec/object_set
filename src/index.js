52c45e9452f0dc83ce5b100dde47f2a1916a34a2
arjunshibu <arjunshibu1999@gmail.com>
2021-01-23 10:26:33 +0530

    Security fix for Prototype Pollution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const setIn = (obj, path, value) => {
   if (path.length == 0 || typeof path.splice == 'undefined' || typeof path[0] == 'undefined') return value;
   let key = path.splice(0, 1);
-  if (typeof obj[key] == 'undefined') obj[key] = {};
+  if (isPrototypePolluted(key) || typeof obj[key] == 'undefined') obj[key] = {};
   obj[key] = setIn(obj[key], path, value);
 
   return obj;
 };
+
+const isPrototypePolluted = (key) => /__proto__|constructor|prototype/.test(key);
 
 export default setIn;
